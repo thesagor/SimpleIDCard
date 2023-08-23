e753212cba836d995d16f4f18b3ff9a40c4f1fc4
Sagor Ahmed <nilsagor004@gmail.com>
2023-08-23 10:22:27 +0600

    function with form

--- func.php
+++ func.php
@@ -1,8 +1,10 @@
 <?php
     //include 'form.php';
+    if(isset($_POST['submit']));
+    $text= $_POST['texts'];
 
-    function myBio($name, $age, $basa, $school, $aim){
-        return "Hi my name is $name. I am $age years old. I live in $basa. My school is $school. My aim is $aim";
+    function myBio($name, $age, $basa, $school, $aim, $text){
+        return "Hi my name is $name. I am $age years old. I live in $basa. My school is $school. My aim is $aim, The other is NOW <br> $text";
 
     }
-echo strtoupper(myBio('sagor', '22', 'harpora', 'Ideal', 'Freelancer'));
+echo strtoupper(myBio('sagor', '22', 'harpora', 'Ideal', 'Freelancer',$text));
---
+++ paragraph.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="func.php" method="POST">
+         <!-- <input type="text" name="texts"> <br> -->
+        <textarea name="texts" id="" cols="30" rows="10"></textarea>
+        <input name="submit" type="submit" value="submit">   
+    </form>
+</body>
+</html>
