19c71694b9bc428ff57e24b2765d477997242cf0
Sagor Ahmed <nilsagor004@gmail.com>
2023-08-23 07:14:49 +0600

    simple id card generation form

---
+++ bio.php
@@ -0,0 +1,85 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+
+</head>
+<body>
+<body class='bg-grid-line'>
+    <div class='card'>
+    <header>
+        <h1>Skills</h1>
+    </header>
+    <article>
+        <img alt='My Pic' id='thumb' src='https://s.cdpn.io/1202/timpietrusky_on_rampage_small_1.jpg'>
+        <h2>Tim Pietrusky</h2>
+        <div class='area'>
+        <h3>Frontend Development</h3>
+        <ul>
+            <li>
+            Name:
+            </li>
+            <li>
+            Email:
+            </li>
+            <li>
+            Phone: 
+            </li>
+            <li>
+            Website: 
+            </li>
+            <li>
+            ID: 
+            </li>
+        </ul>
+        <h3>Backend Development</h3>
+        <ul>
+            <li>
+            PHP
+            <span class='bar percent-90'></span>
+            </li>
+            <li>
+            Java
+            <span class='bar percent-80'></span>
+            </li>
+            <li>
+            MySQL
+            <span class='bar percent-70'></span>
+            </li>
+        </ul>
+        <h3>Tools</h3>
+        <ul>
+            <li>
+            Photoshop
+            <span class='bar percent-70'></span>
+            </li>
+            <li>
+            Eclipse
+            <span class='bar percent-80'></span>
+            </li>
+            <li>
+            Shell
+            <span class='bar percent-90'></span>
+            </li>
+        </ul>
+        <h3>OS</h3>
+        <ul>
+            <li>
+            Ubuntu
+            <span class='bar percent-80'></span>
+            </li>
+            <li>
+            Windows
+            <span class='bar percent-50'></span>
+            </li>
+        </ul>
+        </div>
+    </article>
+    </div>
+    </body>
+</body>
+</html>
+
---
+++ form.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+
+<body>
+    <div class="container">  
+        <form id="contact" action="bio.php" method="post">
+            <h3>Quick Contact</h3>
+            <h4>Contact us today, and get reply with in 24 hours!</h4>
+            <fieldset>
+            <input placeholder="Your name" type="text" tabindex="1" name="fullname" required autofocus>
+            </fieldset>
+            <fieldset>
+            <input placeholder="Your Email Address" type="email" name="email" tabindex="2" required>
+            </fieldset>
+            <fieldset>
+            <input placeholder="Your Phone Number" type="tel" tabindex="3" name="phone" required>
+            </fieldset>
+            <fieldset>
+            <input placeholder="Your Web Site starts with http://" name="website" type="url" tabindex="4" required>
+            </fieldset>
+            <fieldset>
+            <input placeholder="ID no." name="idcard" type="url" tabindex="5" required>
+            </fieldset>
+            <fieldset>
+            <button name="submit" type="submit" id="contact-submit" data-submit="...Sending">Submit</button>
+            </fieldset>
+        </form>
+    </div>
+</body>
+</html>
+
