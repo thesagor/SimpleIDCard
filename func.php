e7a7515af8df5edbf77f3e76aedf45bb572ee161
Sagor Ahmed <nilsagor004@gmail.com>
2023-08-27 10:30:55 +0600

    Constant learning

--- func.php
+++ func.php
@@ -1,10 +1,13 @@
+
 <?php
-    //include 'form.php';
-    if(isset($_POST['submit']));
-    $text= $_POST['texts'];
+    class constClass{
+        const name= "I am sagor ahmed";
+        public $nameT= "This is taskia";
 
-    function myBio($name, $age, $basa, $school, $aim, $text){
-        return "Hi my name is $name. I am $age years old. I live in $basa. My school is $school. My aim is $aim, The other is NOW <br> $text";
-
+        public function user(){
+            echo self::name;
+        }
     }
-echo strtoupper(myBio('sagor', '22', 'harpora', 'Ideal', 'Freelancer',$text));
+
+    $newname= new constClass();
+    $newname->user();
