3d785e46aa279edb12840464310bab91cbf3b654
Sagor Ahmed <nilsagor004@gmail.com>
2023-09-18 11:07:59 +0600

    Filter variable, input,filter, filter sanitize

--- index.php
+++ index.php
@@ -12,26 +12,31 @@
     <div class="container">
         <div class="row">
             <h3>PHP output</h3>
-            <pre> <?php
-            $fname= $_REQUEST['name'];
-            $mail= $_GET['mail'];
-            
-            if(isset($_GET['submit']) && !empty($fname && $mail)):
-             echo $fname; ?></pre> 
+            <?php 
+                if(isset($_POST['submit'])):
+                    $fname= filter_input(INPUT_POST, 'name', FILTER_SANITIZE_STRING);
+                    $mail= filter_var ( $_POST['mail'], FILTER_SANITIZE_EMAIL);
+                    ?>
+                
 
-             <pre>
-                <?php echo $mail; endif; ?>
-             </pre>
+                <pre>
+                   name: <?php echo $fname; ?>
+                </pre>
+
+                <pre>
+                    mail: <?php echo $mail; ?>
+                </pre>
+                <?php endif; ?>
 
         </div>
         <div class="row">
             <div class="column column-50 column-offset-20">
 
-                <form action="" method="GET">
+                <form action="" method="POST">
                     <label for="name">Full name</label>
-                    <input type="text" name="name" class="name">
+                    <input type="text" name="name" class="name" value="<?php echo $fname;?>" >
                     <label for="mail">Email:</label>
-                    <input type="text" name="mail">
+                    <input type="text" name="mail" value="<?php echo $mail;?>" >
                     <input type="submit" class="button button-outline" placeholder="Submit" value="click" name="submit">
                 </form>
 
