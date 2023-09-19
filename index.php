c3ce5177e3e58e61a6b5a78a2c8165b7f8db1b80
Sagor Ahmed <nilsagor004@gmail.com>
2023-09-19 22:07:34 +0600

    revise the form

--- index.php
+++ index.php
@@ -10,38 +10,41 @@
 </head>
 <body>
     <div class="container">
-        <div class="row">
-            <h3>PHP output</h3>
-            <?php 
-                if(isset($_POST['submit'])):
-                    $fname= filter_input(INPUT_POST, 'name', FILTER_SANITIZE_STRING);
-                    $mail= filter_var ( $_POST['mail'], FILTER_SANITIZE_EMAIL);
-                    ?>
-                
 
-                <pre>
-                   name: <?php echo $fname; ?>
-                </pre>
-
-                <pre>
-                    mail: <?php echo $mail; ?>
-                </pre>
-                <?php endif; ?>
-
-        </div>
         <div class="row">
             <div class="column column-50 column-offset-20">
-
                 <form action="" method="POST">
-                    <label for="name">Full name</label>
-                    <input type="text" name="name" class="name" value="<?php echo $fname;?>" >
-                    <label for="mail">Email:</label>
-                    <input type="text" name="mail" value="<?php echo $mail;?>" >
-                    <input type="submit" class="button button-outline" placeholder="Submit" value="click" name="submit">
-                </form>
 
+                    <label for="name">Name:</label>
+                    <input type="text" name="name">
+
+                    <label for="email">Email:</label>
+                    <input type="email" name="mail" placeholder="email">
+
+                    <input type="submit" value="submit" name="submit">
+                </form>
             </div>
         </div>
     </div>
+    <div class="container">
+        <div class="row">
+            <div class="column column-50">
+                 <ol>
+                    <?php 
+                    
+                        if(isset($_POST['submit'])){
+                            $name= $_POST['name'];
+                            $mail= $_POST['mail'];
+                        ?>
+
+                    <ol>
+                        <li>Name: <?php echo $name ?></li>
+                        <li>email: <?php echo $mail ?></li>
+                    </ol>
+                <?php }; ?>
+
+                </ol>
+            </div>
+        </div>
 </body>
 </html>
